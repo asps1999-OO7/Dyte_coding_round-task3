180928c9dbaa9127432d71f23def4c6b6c283cc5
asps1999-OO7 <59870057+asps1999-OO7@users.noreply.github.com>
2021-07-17 21:55:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,132 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React, { Component } from "react";
+ import { View, Image } from "react-native";
+ 
+ import DocumentScanner from "react-native-document-scanner";
+
+ class YourComponent extends Component {
+  render() {
+    return (
+      <View>
+        <DocumentScanner
+          useBase64
+          saveInAppDocument={false}
+          onPictureTaken={data =>
+            this.setState({
+              image: data.croppedImage,
+              initialImage: data.initialImage,
+              rectangleCoordinates: data.rectangleCoordinates
+            })
+          }
+          overlayColor="rgba(255,130,0, 0.7)"
+          enableTorch={false}
+          brightness={0.3}
+          saturation={1}
+          contrast={1.1}
+          quality={0.5}
+          onRectangleDetect={({ stableCounter, lastDetectionType }) =>
+            this.setState({ stableCounter, lastDetectionType })
+          }
+          detectionCountBeforeCapture={5}
+          detectionRefreshRateInMS={50}
+          onPermissionsDenied={() => console.log("Permissions Denied")}
+        />
+        <Image
+          source={{ uri: `data:image/jpeg;base64,${this.state.image}` }}
+          resizeMode="contain"
+        />
+      </View>
+    );
+  }
+}
+/*const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});*/
+
+export default App;
